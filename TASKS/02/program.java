215660e226ed18a778b6444f0671ca2480db367f
krastich <46087866+krastich@users.noreply.github.com>
2019-01-02 16:32:50 +0300

    add folder TASKS/

---
+++ TASKS/02/program.java
@@ -0,0 +1,18 @@
+class Program {
+	public static void main(String[] args) {
+		int sum = 15867;
+		int f = 5000;
+		int t = 1000;
+		int x = 500;
+		int y = 100;
+		int z = 50;
+		int r = 10;
+		int k = 5;
+		int l = 2;
+		int g = 1;
+		int a = x / y;
+		int b = x % y;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
